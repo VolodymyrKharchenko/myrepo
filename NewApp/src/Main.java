aabd47ed593f95314c89fe346a3a1dbbd24aeb1e
Vladimir Kharchenko <voffka85@gmail.com>
2014-06-16 15:27:46 +0300

    11

--- NewApp/src/Main.java
+++ NewApp/src/Main.java
@@ -1,9 +1,14 @@
+import android.app.Activity;
+import android.os.Bundle;
 
-public class Main {
 
-	public static void main(String[] args) {
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 	}
 
 }
